c0abdc6903e3647141055eb46670b4b7e24f8143
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 01:12:32 +0000

    Adding tarfile member sanitization to extractall()

--- build_openwebtext_pretraining_dataset.py
+++ build_openwebtext_pretraining_dataset.py
@@ -63,7 +63,26 @@ def write_examples(job_id, args):
               example_writer.n_written))
     utils.rmkdir(job_tmp_dir)
     with tarfile.open(os.path.join(owt_dir, fname)) as f:
-      f.extractall(job_tmp_dir)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(f, job_tmp_dir)
     extracted_files = tf.io.gfile.listdir(job_tmp_dir)
     random.shuffle(extracted_files)
     for txt_fname in extracted_files:
